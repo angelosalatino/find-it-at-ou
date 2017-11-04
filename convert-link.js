30a80b9aa13227381550c5a3b314380e3c8fa4de
Angelo Antonio Salatino <angelosalatino@users.noreply.github.com>
2017-11-04 11:58:41 +0000

    Update convert-link.js

--- convert-link.js
+++ convert-link.js
@@ -1,11 +1,22 @@
-function convertLink(presentLink){
+///////////////////////////////////////////////////////
+// This script reroutes your connection to the 
+// Journal via the OU Library Proxy
+///////////////////////////////////////////////////////
 
-    var mod = ".libezproxy.open.ac.uk";
+
+/*
+    function : convertLink -> converts the DOI link into a new link including the OU Library Proxy
+    value    : presentLink -> link of the DOI
+    value    : mod         -> link of the proxy
+    return   : futureLink  -> new edited link
+    
+*/
+function convertLink(presentLink, mod){
 
     var res = presentLink.split("//");
     var rfs = res[1].split("/");
-
-    rfs[0] = rfs[0].concat(mod);
+    
+    rfs[0] = rfs[0].concat("."+mod);
 
     futureLink = "http://"+rfs.join("/");
 
@@ -14,6 +25,9 @@ function convertLink(presentLink){
 
 
 var myLocation = window.location.href;
+var mod = "libezproxy.open.ac.uk";
+    
+if(presentLink.includes(mod)) alert("The link was already giving you access through the OU Library.");
 
 var newLink = convertLink(myLocation);
 
